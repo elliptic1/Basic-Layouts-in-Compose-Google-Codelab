b9d6aa8e7cfa7514249cf79b548efc7c86a588e0
Todd B. Smith <elliptic1@gmail.com>
2023-01-08 13:12:44 -0500

    Design the Home screen

--- app/src/main/java/com/tbse/basiclayoutsincompose/MainActivity.kt
+++ app/src/main/java/com/tbse/basiclayoutsincompose/MainActivity.kt
@@ -27,7 +27,10 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -41,7 +44,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tbse.basiclayoutsincompose.ui.theme.MySootheTheme
-import androidx.compose.foundation.lazy.items
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -196,9 +198,22 @@ fun HomeSection(
 }
 
 // Step: Home screen - Scrolling
+
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    // Implement composable here
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
+    ) {
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+    }
 }
 
 // Step: Bottom navigation - Material
@@ -287,6 +302,7 @@ fun HomeSectionPreview() {
         }
     }
 }
+
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun ScreenContentPreview() {
